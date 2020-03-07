b110a38f70042f753969185ced3cccb5f9e330f9
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2020-03-07 15:07:37 -0500

    Update checkPalindrome.scm

--- Asssignments/A6-Scheme/checkPalindrome.scm
+++ Asssignments/A6-Scheme/checkPalindrome.scm
@@ -1,3 +1,7 @@
 (define (isPalindrome? string)
   (let ((chars (string->list string)))
     (equal? chars (reverse chars))))
+
+;;sample run: (isPalindrome? "cbfhsdbh")  ;;#f
+;;(isPalindrome? "abccba")        ;;#true
+
