749468a1950d0dfe82c259a9c66bb73d67e3bafd
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2020-03-07 15:11:16 -0500

    Update ErrorCheck.scm

--- Asssignments/A6-Scheme/ErrorCheck.scm
+++ Asssignments/A6-Scheme/ErrorCheck.scm
@@ -5,3 +5,9 @@
           ((list? x) "List")
           ((char? x) "Char")
           ((boolean? x)  "Boolean"))))
+
+;;sample run: (type #f)    return "Boolean"
+;;(type "abc")  return "String"
+;;(type 3) return "Num"
+;;(type #\A) return "Char"
+;;(type '(1 2 3 4)) return "List"
