4b5581580f1117885b2a224ce6e39d7bb7e0222c
jchen64581 <46164783+jchen64581@users.noreply.github.com>
2020-04-15 23:44:33 -0400

    Add files via upload

---
+++ Asssignments/A10/map.js
@@ -0,0 +1,7 @@
+
+      function initMap() {
+       var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.6253, lng: -73.9515},
+          zoom: 10
+        });
+      }
